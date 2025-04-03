18f8ad7de7139162977dee3b331083afba62c55c
robertIsaac <dev.robert.isaac@gmail.com>
2025-04-03 21:44:08 +0300

    feat: switch to jest esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from 'jest';
 import presets from 'jest-preset-angular/presets';
 
+const esmPreset = presets.createEsmPreset();
+
 export default {
-  ...presets.createCjsPreset(),
+  ...esmPreset,
+  moduleNameMapper: {
+    ...esmPreset.moduleNameMapper,
+    '^rxjs': '<rootDir>/node_modules/rxjs/dist/bundles/rxjs.umd.js',
+  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 } satisfies Config;
